d04fcf8e0eefa00a02aa92c5dfe342c87e4f4885
YeagerCS <lejsmalisi0@gmail.com>
2024-03-17 18:19:07 +0100

    Removed unnecessary files

--- external/client/src/App.jsx
+++ external/client/src/App.jsx
@@ -1,9 +1,4 @@
 import { useState } from 'react'
-import Terminal from './components/Terminal'
-import {DecryptCaesarCipher, EncryptCaesarCipher} from './services/CaesarCipher'
-import Encrypt from './components/Encrypt'
-import Decrypt from './components/Decrypt'
-import IPReceiver from './components/IPReceiver'
 import Router from './router/Router'
 
 function App() {
--- external/client/src/router/Router.jsx
+++ external/client/src/router/Router.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import IPReceiver from '../components/IPReceiver'
 import Terminal from '../components/Terminal'
-import Wallpaper from '../components/Wallpaper'
-import CaesarDashboard from '../components/CaesarDashboard'
 
 export default function Router() {
     return (
@@ -11,8 +9,6 @@ export default function Router() {
             <Routes>
                 <Route exact path='/' element={<IPReceiver/>}/>
                 <Route path="/terminal" element={<Terminal/>}/>
-                <Route path="/the" element={<Wallpaper/>}/>
-                <Route path='/caesar' element={<CaesarDashboard/>}/>
             </Routes>
         </BrowserRouter>
     )
